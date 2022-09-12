96c6bb79cbd0f71573e27ae55adc4b4f1cc3156d
Cat Le <v.catlna@vinai.io>
2022-09-12 05:19:42 +0000

    ! modify & build successfully in devcontainers

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 PROJECT_ROOT=$(pwd)
 WASM_OUT_DIR=${PROJECT_ROOT}/dist
@@ -12,7 +12,7 @@ echo $(pwd)
 mkdir -p ${EMBIND_BUILD_DIR}
 cd ${EMBIND_BUILD_DIR}
 
-if [ $1 == Release ]; then
+if [ $1 = "Release" ]; then
   BUILD_TYPE=Release
 else
   BUILD_TYPE=Debug
