cbfc1bef12af49ca18605c6fdba3f6332f6fb22e
luoxuhai <15186743693@163.com>
2022-09-21 22:56:00 +0800

    feat: add new io apis

--- tests/savePCDData.test.ts
+++ tests/savePCDData.test.ts
@@ -18,28 +18,34 @@ beforeAll(async () => {
   }
 });
 
-describe('savePCDFile', () => {
-  it('should save a ascii PCD file', () => {
+describe('savePCDData', () => {
+  it('should save a ascii PCD data', () => {
     const pcl = (window as any).pcl as PCL.PCLInstance;
 
-    const result = pcl.io.savePCDDataASCII(cloud);
+    const status = pcl.io.savePCDFileASCII('ascii.pcd', cloud);
 
-    expect(result.byteLength).toBe(326);
+    expect(status).toBe(true);
+    expect(pcl.fs.stat('ascii.pcd').isFile).toBe(true);
   });
 
-  it('should save a binary PCD file', () => {
+  it('should save a binary PCD data', () => {
     const pcl = (window as any).pcl as PCL.PCLInstance;
 
-    const result = pcl.io.savePCDDataBinary(cloud);
+    const status = pcl.io.savePCDFileBinary('binary.pcd', cloud);
 
-    expect(result.byteLength).toBe(224);
+    expect(status).toBe(true);
+    expect(pcl.fs.stat('binary.pcd').isFile).toBe(true);
   });
 
-  it('should save a binary_compressed PCD file', () => {
+  it('should save a binary_compressed PCD data', () => {
     const pcl = (window as any).pcl as PCL.PCLInstance;
 
-    const result = pcl.io.savePCDDataBinaryCompressed(cloud);
+    const status = pcl.io.savePCDFileBinaryCompressed(
+      'binary_compressed.pcd',
+      cloud,
+    );
 
-    expect(result.byteLength).toBe(245);
+    expect(status).toBe(true);
+    expect(pcl.fs.stat('binary_compressed.pcd').isFile).toBe(true);
   });
 });
