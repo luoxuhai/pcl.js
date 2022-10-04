43982f92dd720f7fb98e2c8ca86efdf6274b85b0
Darkce <darkce97@gmail.com>
2022-10-04 10:55:45 +0800

    feat: add more point type

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,3 +3,9 @@ declare const __PCLCore__: Emscripten.Module;
 interface Window {
   __PCLCore__: Emscripten.Module;
 }
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I,
+) => void
+  ? I
+  : never;
--- src/modules/common/point-types.ts
+++ src/modules/common/point-types.ts
@@ -1,11 +1,5 @@
-export class Point {
-  constructor(public x: number, public y: number) {}
-}
-
-export class PointXY extends Point {
-  constructor(x = 0, y = 0) {
-    super(x, y);
-  }
+export class PointXY {
+  constructor(public x = 0, public y = 0) {}
 }
 
 export class PointXYZ extends PointXY {
@@ -16,6 +10,18 @@ export class PointXYZ extends PointXY {
 
 export class PointXYZI extends PointXYZ {
   constructor(x = 0, y = 0, z = 0, public intensity = 0) {
+    super(x, y, z);
+  }
+}
+
+export class PointXYZL extends PointXYZ {
+  constructor(x?: number, y?: number, z?: number, public label = 0) {
+    super(x, y, z);
+  }
+}
+
+export class InterestPoint extends PointXYZ {
+  constructor(x?: number, y?: number, z?: number, public strength = 0) {
     super(x, y, z);
   }
 }
@@ -37,6 +43,20 @@ export class PointXYZRGBA extends PointXYZ {
     public a = 0,
   ) {
     super(x, y, z);
+  }
+}
+
+export class PointXYZRGBL extends PointXYZRGB {
+  constructor(
+    x?: number,
+    y?: number,
+    z?: number,
+    r?: number,
+    g?: number,
+    b?: number,
+    public label = 0,
+  ) {
+    super(x, y, z, r, g, b);
   }
 }
 
@@ -63,43 +83,124 @@ export class PointNormal extends PointXYZ {
   }
 }
 
+export class PointXYZLNormal extends PointNormal {
+  constructor(
+    x?: number,
+    y?: number,
+    z?: number,
+    public label = 0,
+    normalX?: number,
+    normalY?: number,
+    normalZ?: number,
+    curvature?: number,
+  ) {
+    super(x, y, z, normalX, normalY, normalZ, curvature);
+  }
+}
+
+export class PointXYZINormal extends PointNormal {
+  constructor(
+    x?: number,
+    y?: number,
+    z?: number,
+    public intensity = 0,
+    normalX?: number,
+    normalY?: number,
+    normalZ?: number,
+    curvature?: number,
+  ) {
+    super(x, y, z, normalX, normalY, normalZ, curvature);
+  }
+}
+
+export class PointXYZRGBNormal extends PointNormal {
+  constructor(
+    x?: number,
+    y?: number,
+    z?: number,
+    public r = 0,
+    public g = 0,
+    public b = 0,
+    normalX?: number,
+    normalY?: number,
+    normalZ?: number,
+    curvature?: number,
+  ) {
+    super(x, y, z, normalX, normalY, normalZ, curvature);
+  }
+}
+
+export class PointSurfel extends PointXYZ {
+  constructor(
+    x?: number,
+    y?: number,
+    z?: number,
+    public normalX = 0,
+    public normalY = 0,
+    public normalZ = 0,
+    public r = 0,
+    public g = 0,
+    public b = 0,
+    public a = 0,
+    public radius = 0,
+    public confidence = 0,
+    public curvature = 0,
+  ) {
+    super(x, y, z);
+  }
+}
+
 export type PointTypesUnion =
+  | PointXY
   | PointXYZ
   | PointXYZI
+  | InterestPoint
+  | PointXYZL
   | PointXYZRGB
   | PointXYZRGBA
+  | PointXYZRGBL
   | Normal
-  | PointNormal;
+  | PointNormal
+  | PointXYZRGBNormal
+  | PointXYZINormal
+  | PointXYZLNormal
+  | PointSurfel;
 
 export type TPointTypesUnion =
+  | typeof PointXY
   | typeof PointXYZ
   | typeof PointXYZI
+  | typeof InterestPoint
+  | typeof PointXYZL
   | typeof PointXYZRGB
   | typeof PointXYZRGBA
+  | typeof PointXYZRGBL
   | typeof Normal
-  | typeof PointNormal;
+  | typeof PointNormal
+  | typeof PointXYZRGBNormal
+  | typeof PointXYZINormal
+  | typeof PointXYZLNormal
+  | typeof PointSurfel;
 
-export type PointTypesIntersection = PointXYZ &
-  PointXYZI &
-  PointXYZRGB &
-  PointXYZRGBA &
-  Normal &
-  PointNormal;
+export type PointTypesIntersection = UnionToIntersection<PointTypesUnion>;
 
-export type TPointTypesIntersection = typeof PointXYZ &
-  typeof PointXYZI &
-  typeof PointXYZRGB &
-  typeof PointXYZRGBA &
-  typeof Normal &
-  typeof PointNormal;
+export type TPointTypesIntersection = UnionToIntersection<TPointTypesUnion>;
 
 export const pointTypeMap = {
+  PointXY,
   PointXYZ,
   PointXYZI,
+  InterestPoint,
+  PointXYZL,
   PointXYZRGB,
   PointXYZRGBA,
+  PointXYZRGBL,
   Normal,
   PointNormal,
+  PointXYZRGBNormal,
+  PointXYZINormal,
+  PointXYZLNormal,
+  PointSurfel,
 };
 
 export abstract class NativeObject {
